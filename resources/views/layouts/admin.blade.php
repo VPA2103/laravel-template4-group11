fe2dbd8e84aa6f4aab53e3e96e0ee509d18cdf08
Loi <nguyenduyloi05012005@mail.com>
2025-10-11 21:57:08 +0700

    admin details

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,17 +12,17 @@
     <title>SB Admin 2 - Bootstrap Admin Theme</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="assets/admin/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('assets/admin/bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="assets/admin/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+    <link href="{{ asset('assets/admin/bower_components/metisMenu/dist/metisMenu.min.css') }}" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="assets/admin/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="{{ asset('assets/admin/dist/css/sb-admin-2.css') }}" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="assets/admin/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
+    <link href="{{ asset('assets/admin/bower_components/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+    @yield('linkcss')
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -378,16 +378,17 @@
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="assets/admin/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="{{ asset('assets/admin/bower_components/jquery/dist/jquery.min.js') }}"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="assets/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="{{ asset('assets/admin/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="assets/admin/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+    <script src="{{ asset('assets/admin/bower_components/metisMenu/dist/metisMenu.min.js') }}"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="assets/admin/dist/js/sb-admin-2.js"></script>
+    <script src="{{ asset('assets/admin/dist/js/sb-admin-2.js') }}"></script>
+    @yield('linkjs')
 
 </body>
 
