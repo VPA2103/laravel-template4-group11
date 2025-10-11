c84c0946f2a7f95bfd65a0b4592e6d73e42f6640
thangvu12321 <vu71394@gmail.com>
2025-10-11 19:13:55 +0700

    da lam xong chia user va admnin va da hien loaisanpham lên trang chủ

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,22 +1,34 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\LoaiSanPham;
 use App\Models\SanPham;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+     public function index(){
         $sanphams = SanPham::all(); // Lấy toàn bộ sản phẩm
         $topsellers = SanPham::take(3)->get();
-        return view('index', compact('sanphams', 'topsellers'));
+        $LoaiSanPhams=LoaiSanPham::take(4)->get();
+        return view('index', compact('sanphams','LoaiSanPhams', 'topsellers'));
     }
     public function show($MaSanPham)
     {
         $sanpham = SanPham::findOrFail($MaSanPham);
         return view('single-product', compact('sanpham'));
     }
-    
-
 
 }
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app') {{-- Nếu bạn dùng layout chung --}}
+
+@section('content')
+
+<h1>Admin Doasboard</h1>
+@endsection
